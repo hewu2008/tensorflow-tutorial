175ab323149272d94f7a25fa92808c59673b7dff
hewu2008 <hewu2008@gmail.com>
2018-04-27 17:44:55 +0800

    完成去噪编码器的开发

--- tutorial/chapter04-autoencoder-mlp/auto_encoder.py
+++ tutorial/chapter04-autoencoder-mlp/auto_encoder.py
@@ -12,7 +12,6 @@ def xavier_init(fan_in, fan_out, constant = 1):
     return tf.random_uniform((fan_in, fan_out), minval=low,
                              maxval=high, dtype=tf.float32)
 
-
 class AdditiveGaussianNosieAutoencoder(object):
     def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus,
                  optimizer=tf.train.AdamOptimizer(), scale=0.1):
@@ -41,3 +40,27 @@ class AdditiveGaussianNosieAutoencoder(object):
         all_weights['b2'] = tf.Variable(tf.zeros([self.n_input], dtype=tf.float32))
         return all_weights
 
+    def partial_fit(self, X):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X, self.scale: self.training_scale})
+        return cost
+
+    def calc_total_cost(self, X):
+        return self.sess.run(self.cost, feed_dict={self.x: X, self.scale: self.training_scale})
+
+    def transform(self, X):
+        return self.sess.run(self.hidden, feed_dict={self.x: X, self.scale : self.training_scale})
+
+    def generate(self, hidden=None):
+        if hidden is None:
+            hidden = np.random.normal(size=self.weights['b1'])
+        return self.sess.run(self.reconstruction, feed_dict={self.hidden: hidden})
+
+    def reconstruct(self, X):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: X, self.scale: self.training_scale})
+
+
+    def getWeight(self):
+        return self.sess.run(self.weights['w1'])
+
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
