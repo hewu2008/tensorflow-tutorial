4aa7f9b4315b4067bf3eb3c3c5678c37b1b455f5
hewu2008 <hewu2008@gmail.com>
2018-05-03 08:39:59 +0800

    添加去噪编码器的调用

--- tutorial/chapter04-autoencoder-mlp/auto_encoder.py
+++ tutorial/chapter04-autoencoder-mlp/auto_encoder.py
@@ -6,11 +6,12 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-def xavier_init(fan_in, fan_out, constant = 1):
+def xavier_init(fan_in, fan_out, constant=1):
     low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
     high = constant * np.sqrt(6.0 / (fan_in + fan_out))
     return tf.random_uniform((fan_in, fan_out), minval=low,
                              maxval=high, dtype=tf.float32)
+
 
 class AdditiveGaussianNosieAutoencoder(object):
     def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus,
@@ -23,7 +24,7 @@ class AdditiveGaussianNosieAutoencoder(object):
         self.weights = self._initialize_weights()
         self.x = tf.placeholder(tf.float32, [None, self.n_input])
         self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input,)),
-                                           self.weights['w1']), self.weights['b1']))
+                                                     self.weights['w1']), self.weights['b1']))
         self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
         self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
         self.optimizer = optimizer.minimize(self.cost)
@@ -48,7 +49,7 @@ class AdditiveGaussianNosieAutoencoder(object):
         return self.sess.run(self.cost, feed_dict={self.x: X, self.scale: self.training_scale})
 
     def transform(self, X):
-        return self.sess.run(self.hidden, feed_dict={self.x: X, self.scale : self.training_scale})
+        return self.sess.run(self.hidden, feed_dict={self.x: X, self.scale: self.training_scale})
 
     def generate(self, hidden=None):
         if hidden is None:
@@ -58,9 +59,44 @@ class AdditiveGaussianNosieAutoencoder(object):
     def reconstruct(self, X):
         return self.sess.run(self.reconstruction, feed_dict={self.x: X, self.scale: self.training_scale})
 
-
     def getWeight(self):
         return self.sess.run(self.weights['w1'])
 
     def getBiases(self):
         return self.sess.run(self.weights['b1'])
+
+
+def standard_scale(X_train, X_test):
+    preprocessor = prep.StandardScaler().fit(X_train)
+    X_train = preprocessor.transform(X_train)
+    X_test = preprocessor.transform(X_test)
+    return X_train, X_test
+
+
+def get_random_block_from_data(data, batch_size):
+    start_index = np.random.randint(0, len(data) - batch_size)
+    return data[start_index:(start_index + batch_size)]
+
+
+if __name__ == "__main__":
+    mnist = input_data.read_data_sets("../MNIST_data", one_hot=True)
+    X_train, X_test = standard_scale(mnist.train.images, mnist.test.images)
+    n_samples = int(mnist.train.num_examples)
+    training_epochs = 20
+    batch_size = 128
+    display_step = 1
+    auto_encoder = AdditiveGaussianNosieAutoencoder(n_input=784,
+                                                    n_hidden=200,
+                                                    transfer_function=tf.nn.softplus,
+                                                    optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+                                                    scale=0.01)
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(n_samples / batch_size)
+        for i in range(total_batch):
+            batch_xs = get_random_block_from_data(X_train, batch_size)
+            cost = auto_encoder.partial_fit(batch_xs)
+            avg_cost += cost / n_samples * batch_size
+        if epoch % display_step == 0:
+            print("Epoch:", "%04d" % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+        print("Total Cost: " + str(auto_encoder.calc_total_cost(X_test)))
