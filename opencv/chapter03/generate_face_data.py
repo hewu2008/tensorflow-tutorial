580d3ee0ec7d126e7518ececb118fa8eb98617de
hewu2008 <hewu2008@gmail.com>
2018-07-12 10:34:54 +0800

    增加人脸图像的获取

---
+++ opencv/chapter03/generate_face_data.py
@@ -0,0 +1,32 @@
+
+# encoding:utf8
+
+import cv2
+
+
+def generate():
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    camera = cv2.VideoCapture(0)
+    count = 0
+    while True:
+        ret, frame = camera.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            f = cv2.resize(gray[y: y + h, x: x + w], (200, 200))
+            cv2.imwrite("%s.pgm" % count, f)
+            count += 1
+
+        cv2.imshow("camera", frame)
+
+        if cv2.waitKey(10) != -1:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    generate()
