c6e4e9b0e4b995ec385a04b48f4d79c2f66158f0
hewu2008 <hewu2008@gmail.com>
2018-07-12 16:34:50 +0800

    添加sift示例

---
+++ opencv/chapter06/sift.py
@@ -0,0 +1,22 @@
+# encoding:utf8
+
+import cv2
+
+if __name__ == "__main__":
+    camera = cv2.VideoCapture(0)
+    while True:
+        ret, img = camera.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        sift = cv2.xfeatures2d.SIFT_create()
+        keypoints, descriptor = sift.detectAndCompute(gray, None)
+
+        img = cv2.drawKeypoints(image=img, outImage=img, keypoints=keypoints,
+                                flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+                                color=(51, 163, 236))
+
+        cv2.imshow("sift_keypoints", img)
+        if cv2.waitKey(10) != -1:
+            break
+    camera.release()
+    cv2.destroyAllWindows()
